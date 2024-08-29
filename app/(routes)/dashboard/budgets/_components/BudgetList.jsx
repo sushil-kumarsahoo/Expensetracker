c9d577a221bf0c6e65a0ec833b7a2d24a1f31dda
sushil-kumarsahoo <suhilkmrsho@gmail.com>
2024-08-30 01:10:12 +0530

    Addded budget list to Budget page

--- app/(routes)/dashboard/budgets/_components/BudgetList.jsx
+++ app/(routes)/dashboard/budgets/_components/BudgetList.jsx
@@ -1,14 +1,49 @@
-import React from 'react'
-import CreateBudget from './CreateBudget'
+"use client";
+import React, { useEffect, useState } from "react";
+import CreateBudget from "./CreateBudget";
+import { db } from "../../../../../utils/dbConfig";
+import { eq, getTableColumns, sql } from "drizzle-orm";
+import { Budgets, Expenses } from "../../../../../utils/schema";
+import { useUser } from "@clerk/nextjs";
+import { index, Index, IndexBuilder } from "drizzle-orm/mysql-core";
+import BudgetItem from "./BudgetItem";
+
 function BudgetList() {
+  const [budgetList, setBudgetList] = useState([]);
+
+  const { user } = useUser();
+
+  useEffect(() => {
+    user && getBudgetList();
+  }, [user]);
+
+  const getBudgetList = async () => {
+    const result = await db
+      .select({
+        ...getTableColumns(Budgets),
+        //totalSpend: sqlsum(${Expenses.amount}).mapWith(Number),
+        totalSpend: sql`sum(CAST(${Expenses.amount} AS NUMERIC))`.mapWith(
+          Number
+        ), // Cast to NUMERIC
+        totalItem: sql`count(${Expenses.id})`.mapWith(Number),
+      })
+      .from(Budgets)
+      .leftJoin(Expenses, eq(Budgets.id, Expenses.budgetId))
+      .where(eq(Budgets.createdBy, user?.primaryEmailAddress?.emailAddress))
+      .groupBy(Budgets.id);
+    setBudgetList(result);
+  };
+
   return (
-    <div className='mt-7'>
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
-        <CreateBudget/>
-        </div>
-      
+    <div className="mt-7">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+        <CreateBudget />
+        {budgetList.map((budget, index) => (
+          <BudgetItem budget={budget} />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default BudgetList
+export default BudgetList;
